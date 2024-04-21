3984adfe252edce59524317e4a5a41d934f58481
puji29 <pujilaksono608@gmail.com>
2024-04-21 12:13:01 +0700

    header

--- app/page.js
+++ app/page.js
@@ -4,8 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>klik me</Button>
-      <h1>hello</h1>
+      Home
     </div>
   );
 }
